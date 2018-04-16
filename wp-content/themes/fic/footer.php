277abe77b9352149fab1a840760f6dc28848b76a
ilam <hima14000@gmail.com>
2018-04-16 16:20:34 +0200

    modif

--- wp-content/themes/fic/footer.php
+++ wp-content/themes/fic/footer.php
@@ -20,9 +20,9 @@
 						</div>
 
 						<div class="col-sm-12 col-lg-8 ">
-							<p class="address" >Miason des jeunes et de la culture- 26 boulevard des  Capucines-onet-le-Château-BP 3425-12034 Rodez</br>
-							Horaires d'ouverture en période scolaire:Lundi 14h-18h, du mardi au vendredi 9h-12 et 12 et 14h-19h, Samedi 9h-12h</br>
-							Horaires d'ouverture en période de vacances scolaires: Du lundi au vendredi:9h-12h et 14h-18h</p>
+							<p class="address" >Miason des jeunes et de la culture- <br />26 boulevard des Capucines Onet-le-Château-BP&nbsp;3425-12034 Rodez</br>
+							Horaires d'ouverture en période scolaire:Lundi 14h-18h,<br /> du mardi au vendredi 9h-12 et 12 et 14h-19h, Samedi 9h-12h</br>
+							Horaires d'ouverture en période de vacances scolaires: <br />Du lundi au vendredi:9h-12h et 14h-18h</p>
 						</div>
 						<div class="col-sm-12 col-lg-2 ">
 						</div>
@@ -31,6 +31,9 @@
 
 				</div>
 			</section>
+			<div>
+				<p>Copyright : fic_onet le chateau/ simplon 2018</p>
+			</div>
 		</footer>
 	<?php wp_footer();?>
 </body>
