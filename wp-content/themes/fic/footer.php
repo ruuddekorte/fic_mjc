892b30873c2fb0c5eb746002bdcbafde04cc64aa
simplon <dekorte.tsrit@gmail.com>
2018-04-16 11:51:59 +0200

    nettoyé index.php et fic.js, en plus recuperé la footer, yahooogit add .

--- wp-content/themes/fic/footer.php
+++ wp-content/themes/fic/footer.php
@@ -6,7 +6,7 @@
 							<img src="<?php echo get_template_directory_uri();?>/img/logos/LOGO-FIC.png" alt="" class="img-responsive img_section"/>
 						</div>
 						<div class="col-sm-12 col-lg-4">
-							<img src="<?php echo get_template_directory_uri();?>/img/logos/logos-mjc-onet.png" alt="" class="img-responsive img_section"/>
+							<img src="<?php echo get_template_directory_uri();?>/img/logos/logo-mjc-onet.png" alt="" class="img-responsive img_section"/>
 						</div>
 						<div class="col-sm-12 col-lg-4">
 							<img src="<?php echo get_template_directory_uri();?>/img/logos/LOGO-CIEC.png" alt="" class="img-responsive img_section"/>
