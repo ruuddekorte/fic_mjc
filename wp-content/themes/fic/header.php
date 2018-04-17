a7313d01821ac992557ac3290ff984dd54180a55
vanessa <vanessa.chaillou@orange.fr>
2018-04-17 10:03:38 +0200

    menu wp / formulaire de contact

--- wp-content/themes/fic/header.php
+++ wp-content/themes/fic/header.php
@@ -16,20 +16,7 @@
 		<div class="pos-f-t">
 			<div class="collapse" id="navbarToggleExternalContent">
 				<div class="bg-dark p-4">
-					<div class="row">
-						<div class="col-sm-12 col-lg-3 menu">
-							<a class="navbar-brand" href="http://localhost:8888/"><img src="<?php echo get_template_directory_uri();?>/img/logos/LOGO-FIC.png" alt="" class="img-responsive img_menu"/></a>
-						</div>
-						<div class="col-sm-12 col-lg-3 menu">
-							<a class="navbar-text" href="#">Le Projet Fabrique</a>
-						</div>
-						<div class="col-sm-12 col-lg-3 menu">
-							<a class="navbar-text" href="#">Proposez une initiative</a>
-						</div>	
-						<div class="col-sm-12 col-lg-3 menu">
-							<a class="navbar-text" href="http://localhost:8888/index.php/contact/">Contact</a>
-						</div>
-					</div>			
+					<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
 				</div>
 			</div>
 			<nav class="navbar navbar-dark bg-dark">
