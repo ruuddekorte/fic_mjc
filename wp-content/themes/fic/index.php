1979b4304d7e23f967bc8ac10ac834ef20a2626a
simplon <dekorte.tsrit@gmail.com>
2018-04-19 15:01:15 +0200

    une option nul pour une action nul

--- wp-content/themes/fic/index.php
+++ wp-content/themes/fic/index.php
@@ -4,42 +4,36 @@
  *
  */
 
-	define( 'WP_USE_THEMES', false ); get_header(); 
+define( 'WP_USE_THEMES', false ); get_header(); 
 ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<?php if ( have_posts() ) : ?>
+				<?php if ( is_home() && ! is_front_page() ) : ?>
+					<header>
+						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					</header>
+				<?php endif; 
+				while ( have_posts() ) : the_post();
+				// Start the loop.
+					get_template_part( 'content', get_post_format() );
+				// End the loop.
+				endwhile;
 
-		<?php if ( have_posts() ) : ?>
-
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+				// Previous/next page navigation.
+				the_posts_pagination( array(
+					'prev_text'          => __( 'Previous page', 'fic' ),
+					'next_text'          => __( 'Next page', 'fic' ),
+					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'fic' ) . ' </span>',
+				) );
+			// If no content, show "No posts found".
+			else : ?> 
+				<h2 class="center">Not Found</h2>
+				<p class="center">
+				<?php _e("Sorry, but you are looking for something that isn't here."); ?>
+				</p>
 			<?php endif; ?>
-
-			<?php
-			// Start the loop.
-			while ( have_posts() ) : the_post();
-				get_template_part( 'content', get_post_format() );
-			// End the loop.
-			endwhile;
-
-			// Previous/next page navigation.
-			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'fic' ),
-				'next_text'          => __( 'Next page', 'fic' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'fic' ) . ' </span>',
-			) );
-
-		// If no content, show "No posts found".
-		else : ?> 
-			<h2 class="center">Not Found</h2>
-			<p class="center">
-			<?php _e("Sorry, but you are looking for something that isn't here."); ?>
-			</p>
-		<?php endif; ?>
-
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
 
--- wp-content/themes/fic/page.php
+++ wp-content/themes/fic/page.php
@@ -1,23 +1,19 @@
 <?php get_header(); ?>
 
 <section id="contenu">
-	<!-- lister les article wordpress -->
-<?php
-	if (have_posts()) { 
-		while (have_posts() ) {
-	    	the_post();
-		?>
-		<article class="initiative_page">
-
-			<h2><?php the_title();?></h2>
-
-			<?php the_content();?>
-		</article>
-		<?php	    	
-		}
-	}
-	?>
+	<main>
+		<!-- lister les article/page wordpress -->
+		<?php if (have_posts()) { 
+			while (have_posts() ) {
+		    	the_post(); ?>
+				<article class="initiative_page">
+					<h2><?php the_title();?></h2>
+					<?php the_content();?>
+				</article>
+			<?php	    	
+			}
+		} ?>
+	</main>
 </section>
 
-<?php get_template_part('sidebar');?>
 <?php get_footer(); ?>
