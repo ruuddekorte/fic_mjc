cd19beeae6a10d46ed470d33418266d8c1af6d8c
vanessa <vanessa.chaillou@orange.fr>
2018-04-20 14:51:59 +0200

    error page.php

--- wp-content/themes/fic/page.php
+++ wp-content/themes/fic/page.php
@@ -1,15 +1,13 @@
 <?php get_header(); ?>
 	<main id="contenu">
-		<?php if (have_posts()){
+		<?php if (have_posts()) {
 			while (have_posts()) {
 			 the_post();
 			?>
-			<article class="initiative_page">			
 				<h2><?php the_title();?></h2>
 				<?php the_content();?>
-			</article>
 			<?php 
-			} 
+			}
 		}
 		?>
 	</main>
